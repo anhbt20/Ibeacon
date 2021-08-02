c8391f89bf41d11a29a580e2205836184c11c10d
Bui <buianhxtb@gmail.com>
2021-08-02 15:45:30 +0700

    Initial Commit

---
+++ Ibeacon2.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ibeacon2.0
+//
+//  Created by MR932 on 02/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
